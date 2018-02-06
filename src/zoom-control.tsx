89bc9b3c0c0382946a35668da8ced4ffdf82f65e
Stephen McMahon <stephen@symbiote.com.au>
2018-02-06 15:48:07 +1100

    Remove console.log

--- src/zoom-control.tsx
+++ src/zoom-control.tsx
@@ -129,7 +129,7 @@ export default class ZoomControl extends React.Component<Props, State> {
       ...buttonStyleMinus,
       ...hover === MINUS ? buttonStyleHovered : {}
     };
-    console.log('TEST tabindex:' + tabIndex);
+
     return (
       <div
         className={className}
